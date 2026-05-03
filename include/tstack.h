4023aef85ebe62d43d2c5d9f328262525ce05a85
saq1rette <165058154+saq1rette@users.noreply.github.com>
2026-05-03 19:51:12 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,49 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
+#ifndef TSTACK_H
+#define TSTACK_H
 
-template<typename T, int size>
+#include <cstddef>
+
+template <typename T, int N>
 class TStack {
-  // добавьте код стека
+private:
+    T data[N];
+    int top_idx;
+
+public:
+    TStack() : top_idx(-1) {}
+    
+    bool isEmpty() const {
+        return top_idx == -1;
+    }
+    
+    bool isFull() const {
+        return top_idx == N - 1;
+    }
+    
+    void push(const T& item) {
+        if (!isFull()) {
+            data[++top_idx] = item;
+        }
+    }
+    
+    T pop() {
+        if (!isEmpty()) {
+            return data[top_idx--];
+        }
+        return T();
+    }
+    
+    T top() const {
+        if (!isEmpty()) {
+            return data[top_idx];
+        }
+        return T();
+    }
+    
+    void clear() {
+        top_idx = -1;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif // TSTACK_H
