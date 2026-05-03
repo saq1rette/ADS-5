aa0777e9df9392efcbe78f462418a8b8aafc9afb
saq1rette <165058154+saq1rette@users.noreply.github.com>
2026-05-03 20:03:29 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,30 @@
-// Copyright 2021 NNTU-CS
 #include <iostream>
-#include "src/alg.cpp"
+#include <string>
+#include "../include/alg.h"
 
 int main() {
-    // Пример 1
-    std::string infix1 = "(2+2)*(3-1)";
-    std::string postfix1 = infx2pstfx(infix1);
-    std::cout << "Infix: " << infix1 << std::endl;
-    std::cout << "Postfix: " << postfix1 << std::endl;
-    std::cout << "Result: " << eval(postfix1) << std::endl;
-    std::cout << std::endl;
-    
-    // Пример 2
-    std::string infix2 = "100+200/2";
-    std::string postfix2 = infx2pstfx(infix2);
-    std::cout << "Infix: " << infix2 << std::endl;
-    std::cout << "Postfix: " << postfix2 << std::endl;
-    std::cout << "Result: " << eval(postfix2) << std::endl;
-    std::cout << std::endl;
-    
-    // Пример 3
-    std::string infix3 = "(10+2)*(3-1)/2";
-    std::string postfix3 = infx2pstfx(infix3);
-    std::cout << "Infix: " << infix3 << std::endl;
-    std::cout << "Postfix: " << postfix3 << std::endl;
-    std::cout << "Result: " << eval(postfix3) << std::endl;
-    
-    return 0;
+  // Пример 1
+  std::string infix1 = "(2+2)*(3-1)";
+  std::string postfix1 = infx2pstfx(infix1);
+  std::cout << "Infix: " << infix1 << std::endl;
+  std::cout << "Postfix: " << postfix1 << std::endl;
+  std::cout << "Result: " << eval(postfix1) << std::endl;
+  std::cout << std::endl;
+
+  // Пример 2
+  std::string infix2 = "100+200/2";
+  std::string postfix2 = infx2pstfx(infix2);
+  std::cout << "Infix: " << infix2 << std::endl;
+  std::cout << "Postfix: " << postfix2 << std::endl;
+  std::cout << "Result: " << eval(postfix2) << std::endl;
+  std::cout << std::endl;
+
+  // Пример 3
+  std::string infix3 = "(10+2)*(3-1)/2";
+  std::string postfix3 = infx2pstfx(infix3);
+  std::cout << "Infix: " << infix3 << std::endl;
+  std::cout << "Postfix: " << postfix3 << std::endl;
+  std::cout << "Result: " << eval(postfix3) << std::endl;
+
+  return 0;
 }
