5522e60ada400ea38016f29e199ce22f516ac727
saq1rette <165058154+saq1rette@users.noreply.github.com>
2026-05-03 20:02:00 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,49 +1,48 @@
-// Copyright 2021 NNTU-CS
-#ifndef TSTACK_H
-#define TSTACK_H
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
 
 #include <cstddef>
 
 template <typename T, int N>
 class TStack {
-private:
-    T data[N];
-    int top_idx;
+ private:
+  T data[N];
+  int top_idx;
 
-public:
-    TStack() : top_idx(-1) {}
-    
-    bool isEmpty() const {
-        return top_idx == -1;
+ public:
+  TStack() : top_idx(-1) {}
+
+  bool isEmpty() const {
+    return top_idx == -1;
+  }
+
+  bool isFull() const {
+    return top_idx == N - 1;
+  }
+
+  void push(const T& item) {
+    if (!isFull()) {
+      data[++top_idx] = item;
     }
-    
-    bool isFull() const {
-        return top_idx == N - 1;
+  }
+
+  T pop() {
+    if (!isEmpty()) {
+      return data[top_idx--];
     }
-    
-    void push(const T& item) {
-        if (!isFull()) {
-            data[++top_idx] = item;
-        }
+    return T();
+  }
+
+  T top() const {
+    if (!isEmpty()) {
+      return data[top_idx];
     }
-    
-    T pop() {
-        if (!isEmpty()) {
-            return data[top_idx--];
-        }
-        return T();
-    }
-    
-    T top() const {
-        if (!isEmpty()) {
-            return data[top_idx];
-        }
-        return T();
-    }
-    
-    void clear() {
-        top_idx = -1;
-    }
+    return T();
+  }
+
+  void clear() {
+    top_idx = -1;
+  }
 };
 
-#endif // TSTACK_H
+#endif  // INCLUDE_TSTACK_H_
